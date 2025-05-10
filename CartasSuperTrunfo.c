4b50233081fb54fc45744fdd301045a27157fd77
RedSword-XT <202504507451@alunos.estacio.br>
2025-05-10 15:38:40 +0000

    Reestruturação do código e adição de comentários e cálculos de densidade populacional e PIB per capita

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,82 +1,127 @@
 #include <stdio.h>
+#include <string.h> //incluído para possibilitar o uso de strcspn
 
 // Desafio Super Trunfo - Países
 
 int main() {
+//Variáveis para armazenar os dados
+    char nome1[50], nome2[50];
+    char estado1, estado2;
+    int codigo1, codigo2;
+    int pop1, pop2;
+    int pontos1, pontos2;
+    float area1, area2;
+    float pib1, pib2;
+    float dens1, dens2;
+    float percapita1, percapita2;
 
-    int populacao1 = 1, populacao2 = 1, pontos1 = 1, pontos2 = 1, codigocidade1 = 10, codigocidade2 = 10;
-    float area1 = 1, area2 = 1, pib1 = 1, pib2 = 1;
-    char codigoestado1 = 'A', codigoestado2 = 'A', estado1[30] = "Paraná", estado2[30] = "Paraná", cidade1[30] = "Macapá", cidade2[30] = "Macapá";
-
-    /*
-    A primeira parte do código será onde o usuário fará o cadastro das cartas,
-    um item por vez, sendo na sequência: nome do estado(código de A à H), código do estado, código da cidade,
-    nome da cidade, população, área, PIB e pontos turísticos.
-    */
+/*
+A primeira parte do código será onde o usuário fará o cadastro das cartas,
+um item por vez, sendo na sequência: letra do estado(de A a H), código do estado,
+nome da cidade, população, área, PIB e pontos turísticos.
+*/
 
 //Texto de introdução
-        printf("Bem vindo ao Super Trunfo!\nPara iniciar, vamos cadastrar duas cartas!\n\n");
+
+    printf("*** Bem vindo ao Super Trunfo! ***\nPara iniciar, vamos cadastrar duas cartas!\n\n");
     
 //Cadastro da primeira carta
 
-     printf("Primeiro, digite o nome do estado: \n");
-     scanf("%s", estado1);
+    printf("Primeiro, digite o a letra do estado da cidade 1 (A a H): \n");
+    scanf("%c", &estado1);
 
-     printf("Digite o código desse estado, uma letra de A à H: \n");
-     scanf("%s", &codigoestado1);
+    printf("Digite o código do estado, um número entre 01 e 99\n");
+    scanf("%i", &codigo1);
+    getchar(); //evita que o programa pule o próximo fgets;
 
-     printf("Digite o código da cidade, um número entre 01 e 99\n");
-     scanf("%i", &codigocidade1);
+    printf("Agora digite o nome da cidade 1:\n");
+    fgets(nome1, 50, stdin); //fgets para que o usuário possa digitar nomes compostos
+    nome1[strcspn(nome1, "\n")] = 0; //remove o \n do comando anterior
 
-     printf("Agora digite o nome da cidade:\n");
-     scanf("%s", cidade1);
+    printf("Agora digite a população da cidade 1: \n");
+    scanf("%i", &pop1);
 
-     printf("Agora digite a população: \n");
-     scanf("%i", &populacao1);
+    printf("Agora digite a área da cidade 1 em km2: \n");
+    scanf("%f", &area1);
 
-     printf("Agora digite a área da cidade: \n");
-     scanf("%f", &area1);
+    printf("Agora digite o PIB da cidade 1 em bilhões: \n");
+    scanf("%f", &pib1);
 
-     printf("Agora digite o PIB da cidade: \n");
-     scanf("%f", &pib1);
-
-     printf("Agora digite os pontos turísticos: \n");
-     scanf("%i", &pontos1);
-
-     printf("\n\nPronto! Agora vamos cadastrar a segunda carta:\n\n\n");
+    printf("Agora digite os pontos turísticos da cidade 1: \n");
+    scanf("%i", &pontos1);
+    getchar(); //Evita que o programa pule o scanf("%c", &estado2);
+    
+    printf("\n\nPronto! Agora vamos cadastrar a segunda carta:\n\n\n");
 
 //Cadastro da segunda carta
 
-     printf("Digite o nome do estado: \n");
-     scanf("%s", estado2);
+    printf("Primeiro, digite o a letra do estado da cidade 2 (A a H): \n");
+    scanf("%c", &estado2);
 
-     printf("Digite o código do estado, uma letra de A à H: \n");
-     scanf("%s", &codigoestado2);
+    printf("Digite o código do estado, um número entre 01 e 99\n");
+    scanf("%i", &codigo2);
+    getchar(); //evita que o programa pule o próximo fgets;
+    
+    printf("Agora digite o nome da cidade 2:\n");
+    fgets(nome2, 50, stdin); //fgets para que o usuário possa digitar nomes compostos
+    nome2[strcspn(nome2, "\n")] = 0; //remove o \n do comando anterior
 
-     printf("Digite o código da cidade, um número entre 01 e 99\n");
-     scanf("%i", &codigocidade2);
+    printf("Agora digite a população da cidade 2: \n");
+    scanf("%i", &pop2);
 
-     printf("Agora digite o nome da cidade:\n");
-     scanf("%s", cidade2);
+    printf("Agora digite a área da cidade 2 em km2: \n");
+    scanf("%f", &area2);
 
-     printf("Agora digite a população: \n");
-     scanf("%i", &populacao2);
+    printf("Agora digite o PIB da cidade 2 em bilhões: \n");
+    scanf("%f", &pib2);
 
-     printf("Agora digite a área da cidade: \n");
-     scanf("%f", &area2);
-
-     printf("Agora digite o PIB da cidade: \n");
-     scanf("%f", &pib2);
-
-     printf("Agora digite os pontos turísticos: \n");
-     scanf("%i", &pontos2);
+    printf("Agora digite os pontos turísticos da cidade 2: \n");
+    scanf("%i", &pontos2);
 
     printf("\n\nPronto! Agora confira as cartas cadastradas:\n\n\n");
 
-//Exibição das informações inseridas pelo usuário, cada comando Printf representando uma carta
 
-        printf("Carta 01:\n\n Código: %c%i \n Estado: %s \n Cidade: %s \n Área: %.2f Km \n População: %i \n PIB: R$ %.2f \n Pontos Turísticos: %i\n\n", codigoestado1, codigocidade1, estado1, cidade1, area1, populacao1, pib1, pontos1);
-        printf("Carta 02:\n\n Código: %c%i \n Estado: %s \n Cidade: %s \n Área: %.2f Km \n População: %i \n PIB: R$ %.2f \n Pontos Turísticos: %i\n\n", codigoestado2, codigocidade2, estado2, cidade2, area2, populacao2, pib2, pontos2);
 
-    return 0;
+/*
+A segunda parte do código é onde será feito o cálculo de densidade populacional
+e PIB per capita,sendo que o PIB será exibido em bilhões, será necessário
+multiplicar o PIB inserido pelo usuário por 1 bilhão (1.000.000.000)
+*/
+
+dens1 = pop1 / area1;
+percapita1 = (pib1 * 1000000000) / pop1;
+
+dens2 = pop2 / area2;
+percapita2 = (pib2 * 1000000000) / pop2;
+
+
+
+//A terceira parte do código é a exibição dos dados para o usuário
+
+    //Carta 1
+    printf("\n*** Dados da Cidade 1 ***\n");
+    printf("Estado: %c\n", estado1);
+    printf("Código: %c%d\n", estado1, codigo1);
+    printf("Nome da Cidade: %s\n", nome1);
+    printf("População: %i\n", pop1);
+    printf("Área: %.2f km2\n", area1);
+    printf("PIB: R$ %.2f bilhões\n", pib1);
+    printf("Pontos Turísticos: %i\n", pontos1);
+    printf("Densidade Populacional: %.2f hab/km2\n", dens1);
+    printf("PIB per capita: %.2f bilhões/hab\n", percapita1);
+
+    //Carta 2
+    printf("\n*** Dados da Cidade 2 ***\n");
+    printf("Estado: %c\n", estado2);
+    printf("Código: %c%d\n", estado2, codigo2);
+    printf("Nome da Cidade: %s\n", nome2);
+    printf("População: %i\n", pop2);
+    printf("Área: %.2f km2\n", area2);
+    printf("PIB: R$ %.2f bilhões\n", pib2);
+    printf("Pontos Turísticos: %i\n", pontos2);
+    printf("Densidade Populacional: %.2f hab/km2\n", dens2);
+    printf("PIB per capita: %.2f bilhões/hab\n", percapita2);
+
+
+return 0;
 }
